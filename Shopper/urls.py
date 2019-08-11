5e66074428454d89a2b7f87a09580cc928ce9d05
Mahabubul Hasan <shourav6688@gmail.com>
2019-08-11 13:32:38 +0600

    initial model created

--- Shopper/urls.py
+++ Shopper/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
+from shopperapp import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/',include('shopperapp.urls')),
     path('accounts/',include('allauth.urls'))
 ]
--- shopperapp/models.py
+++ shopperapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+class ITEM(models.Model):
+    pass
+
+class ORDER_ITEM(models.Model):
+    pass
+
+
+class ORDER(models.Model):
+    pass
